9c52cc919d75241b4f67f0b60b3ca5d5dfbcf32e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-02 22:14:07 -0700

    ✅ Pass test

--- R/transform_stats_players_to_key_passes.R
+++ R/transform_stats_players_to_key_passes.R
@@ -4,3 +4,27 @@ player_stats_to_key_passes <- function(player_stats) {
     dplyr::summarize(key_passes = sum(passes_key, na.rm = TRUE)) |>
     dplyr::ungroup()
 }
+
+match_stats_to_tidy_match<- function(match_stats) {
+  home <- select_home_metrics(match_stats)
+  away <- select_away_metrics(match_stats)
+  rbind(home, away) |>
+    dplyr::rename(team_id = id) |>
+    dplyr::arrange(-match_id)
+}
+
+select_home_metrics <- function(match_stats) {
+  match_stats |>
+    dplyr::select(c(match_id, starts_with("home"))) |>
+    dplyr::rename(goal = home) |>
+    dplyr::mutate(home = TRUE) |>
+    dplyr::rename_with(~ gsub("home_", "", .x, fixed = TRUE))
+}
+
+select_away_metrics <- function(match_stats) {
+  match_stats |>
+    dplyr::select(c(match_id, starts_with("away"))) |>
+    dplyr::rename(goal = away) |>
+    dplyr::mutate(home = FALSE) |>
+    dplyr::rename_with(~ gsub("away_", "", .x, fixed = TRUE))
+}
