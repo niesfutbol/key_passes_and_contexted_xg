2c6bfd620e49b319fd49ac4f1dc9c30bdf3a6d78
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-02 21:46:17 -0700

    🛑🧪 Test `match_stats_to_tidy_match()`

--- tests/testthat/test_transform_stats_players_to_key_passes.R
+++ tests/testthat/test_transform_stats_players_to_key_passes.R
@@ -6,3 +6,12 @@ describe("Make summary of key passes", {
     expect_equal(obtained, expected)
   })
 })
+
+describe("Transform match stats to tidy", {
+  it("First example", {
+    player_stats <- readr::read_csv("/workdir/tests/data/statistics_262_2022_part.csv", show_col_types = FALSE)
+    expected <- readr::read_csv("/workdir/tests/data/statistics_262_2022_part_tidy.csv", show_col_types = FALSE)
+    obtained <- match_stats_to_tidy_match(player_stats)
+    expect_equal(obtained, expected)
+  })
+})
